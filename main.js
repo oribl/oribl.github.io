e13e4c1783ad3c150b21ae3903844a93d85fc85b
oribl <oriblich@gmail.com>
2023-07-28 17:36:35 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
+// Select the small and big cursor elements
 const cursorSmall = document.querySelector('.small');
 const cursorBig = document.querySelector('.big');
 
-
-const positionElement = (e)=> {
+// Function to update the position of the custom cursors based on mouse movement
+const positionElement = (e) => {
+  // Get the Y and X coordinates of the mouse cursor
   const mouseY = e.clientY;
   const mouseX = e.clientX;
    
+  // Move the small cursor to the current mouse position
   cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
   
+  // Move the big cursor to the current mouse position
   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
 }
 
-window.addEventListener('mousemove', positionElement)
+// Add an event listener to detect mouse movement on the window
+window.addEventListener('mousemove', positionElement);
